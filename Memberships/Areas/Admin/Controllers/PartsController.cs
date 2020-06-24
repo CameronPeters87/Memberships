c30383966a198c3fb058d65ae7d1644f0a45b4ea
cameronpeters87 <cameronpeters87@gmail.com>
2020-06-24 16:06:16 +0200

    Added minor Entity Controllers

---
+++ Memberships/Areas/Admin/Controllers/PartsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Memberships.Entities;
+using Memberships.Models;
+
+namespace Memberships.Areas.Admin.Controllers
+{
+    public class PartsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Parts
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Parts.ToListAsync());
+        }
+
+        // GET: Admin/Parts/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = await db.Parts.FindAsync(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // GET: Admin/Parts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Parts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Title")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Parts.Add(part);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(part);
+        }
+
+        // GET: Admin/Parts/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = await db.Parts.FindAsync(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // POST: Admin/Parts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Title")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(part).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(part);
+        }
+
+        // GET: Admin/Parts/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = await db.Parts.FindAsync(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // POST: Admin/Parts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Part part = await db.Parts.FindAsync(id);
+            db.Parts.Remove(part);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Memberships/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Memberships.Entities;
+using Memberships.Models;
+
+namespace Memberships.Areas.Admin.Controllers
+{
+    public class ProductTypesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/ProductTypes
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.ProductTypes.ToListAsync());
+        }
+
+        // GET: Admin/ProductTypes/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductType productType = await db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productType);
+        }
+
+        // GET: Admin/ProductTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/ProductTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Title")] ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductTypes.Add(productType);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(productType);
+        }
+
+        // GET: Admin/ProductTypes/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductType productType = await db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productType);
+        }
+
+        // POST: Admin/ProductTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Title")] ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productType).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(productType);
+        }
+
+        // GET: Admin/ProductTypes/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductType productType = await db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productType);
+        }
+
+        // POST: Admin/ProductTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ProductType productType = await db.ProductTypes.FindAsync(id);
+            db.ProductTypes.Remove(productType);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
