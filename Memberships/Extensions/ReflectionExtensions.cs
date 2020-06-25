8f2e75051750771368175daee0086dee2aba15fa
cameronpeters87 <cameronpeters87@gmail.com>
2020-06-25 14:52:14 +0200

    Added Extenstion methodsm ReflectionExtension and ICollectionExtensions for the dropdown list for items

---
+++ Memberships/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Extensions
+{
+    // For ToSelectListItem
+    public static class ReflectionExtensions
+    {
+        // Extension method
+        // Returns a string of a property that we passed through in the parameter
+        // We work with obj Type T
+        // Generic type T
+        public static string GetPropertyValue<T>(this T item, string propertyName)
+        {
+            return item.GetType()
+                .GetProperty(propertyName)
+                .GetValue(item, null).ToString();
+        }
+    }
+}
