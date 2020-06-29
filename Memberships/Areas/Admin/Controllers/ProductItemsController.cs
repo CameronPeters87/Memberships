2a4b072ef81e4cb66c3f0dabdc9439e41d91bc2a
cameronpeters87 <cameronpeters87@gmail.com>
2020-06-29 13:38:52 +0200

    Updated ProductController Post Edit Method

--- Memberships/Areas/Admin/Controllers/ProductItemsController.cs
+++ Memberships/Areas/Admin/Controllers/ProductItemsController.cs
@@ -90,12 +90,13 @@ namespace Memberships.Areas.Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "ProductId,ItemId")] ProductItem productItem)
+        public async Task<ActionResult> Edit([Bind(Include = "ProductId,ItemId,OldProductId,OldItemId")] ProductItem productItem)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(productItem).State = EntityState.Modified;
-                await db.SaveChangesAsync();
+                var canChange = await productItem.CanChange(db);
+                if (canChange)
+                    await productItem.Change(db);
                 return RedirectToAction("Index");
             }
             return View(productItem);
