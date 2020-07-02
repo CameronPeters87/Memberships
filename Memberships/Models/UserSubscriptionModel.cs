ee821b5f1445ea9448eb623abd7e4f99266efb45
cameronpeters87 <cameronpeters87@gmail.com>
2020-07-02 13:10:04 +0200

    Added UserSubscriptionViewModel and UserSubscriptionModel

---
+++ Memberships/Models/UserSubscriptionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Memberships.Models
+{
+    public class UserSubscriptionModel
+    {
+        public int Id { get; set; }
+        [MaxLength(255)]
+        [Required]
+        public string Title { get; set; }
+        [MaxLength(255)]
+        public string Description { get; set; }
+        [MaxLength(20)]
+        public string RegistrationCode { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
---
+++ Memberships/Models/UserSubscriptionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Memberships.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Models
+{
+    public class UserSubscriptionViewModel
+    {
+        public ICollection<Subscription> Subscriptions { get; set; }
+        public ICollection<UserSubscriptionModel> UserSubscriptionModels { get; set; }
+        public bool DisableDropDownList { get; set; }
+        public string UserId { get; set; }
+    }
+}
